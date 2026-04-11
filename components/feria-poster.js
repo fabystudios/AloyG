a50cfd4000948dbd1a38c9d982939d2fae32cc44
willyEsco <willyEscobar@gmail.com>
2026-04-10 22:34:57 -0300

    fix

--- components/feria-poster.js
+++ components/feria-poster.js
@@ -421,7 +421,7 @@ class FeriaPoster extends HTMLElement {
       }
       .fp-figure--mobile {
         display: block;
-        height: clamp(843px, 240vw, 1347px);
+        height: clamp(562px, 160vw, 898px);
         width: auto;
         bottom: var(--fp-fig-mob-bottom, 0px);
         left: var(--fp-fig-mob-left, -14%);
